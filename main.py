d1ca708ba57891fd86553161b7d799572ae2f138
ipaolo <igor@igor-OptiPlex-7040>
2017-10-08 15:37:06 +0400

    Implemented simple strategy

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ secret = "fe11c3a0a7d14020bb78554ceb67de76"
 
 bittrex = Bittrex(key,secret)
 
-#print(bittrex.get_currencies())
+threshold_activ = 0.7 # Number between -1 and 1
+threshold_off   = -0.2 # Number between -1 and 1
 
-#markets = bittrex.get_markets()
-#print(json.dumps(markets, indent=4, sort_keys=True))
+btc_vault = 1.0 # Btc vault
+usdt_vault = 0 # Usd tether vault
+
+sell_pos = 0
+buy_pos = 0
 
 while True:
 
-	results = bittrex.get_market_history('USDT-BTC',100)['result']
+	results = bittrex.get_market_history('USDT-BTC',800)['result']
 
 	buys = 0
 	sells = 0
@@ -42,11 +46,39 @@ while True:
 			sells_size += i['Quantity']
 
 			if latest_price_bid == 0:
-				latest_price_bid = i['Price']		
+				latest_price_bid = i['Price']	
+
+	# Strategy
+	
+	ratio = (buys_size - sells_size)/(buys_size+sells_size)
+
+	# Check buy 
+	if buy_pos == 0:
+		if ratio > threshold_activ:
+			btc_vault += usdt_vault/latest_price_ask
+			usdt_vault = 0
+			buy_pos = 1
+	elif buy_pos == 1:
+		if ratio < threshold_off:
+			buy_pos = 0
+
+	# Check sell
+	if sell_pos == 0:
+		if ratio < -threshold_activ:
+			usdt_vault += btc_vault*latest_price_bid
+			btc_vault = 0
+			sell_pos = 1
+	elif sell_pos == 1:
+		if ratio > -threshold_off:
+			sell_pos = 1
+
+
 
 	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 	print(" Buys   : %10d %8f" % (buys,buys_size))
 	print(" Sells  : %10d %8f" % (sells,sells_size))
+	print(" Ratio  : %10f" % ratio)
 	print("Bitcoin price: BID:%10f ASK:%10f" % (latest_price_bid, latest_price_ask))
+	print("BTC vault: %8f USDT vault: %8f Total value: %8f" %(btc_vault,usdt_vault,usdt_vault + btc_vault * latest_price_bid))
 	print("")
 	time.sleep(60)
