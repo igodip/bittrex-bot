0def5262a6b406eac345c9de5fb46a046bc08bdb
Igor <igor.di.paolo@gmail.com>
2017-10-09 06:42:52 +0200

    Changed tresholds added max spread

--- main.py
+++ main.py
@@ -4,76 +4,80 @@ import time
 from datetime import datetime
 
 import requests.packages.urllib3
+
 requests.packages.urllib3.disable_warnings()
 
 import sqlite3
+
 conn = sqlite3.connect('bitcoin.db')
 
 key = "230cd2aaff354c43a3219a978ac2aebd"
 secret = "fe11c3a0a7d14020bb78554ceb67de76"
 
-bittrex = Bittrex(key,secret)
+bittrex = Bittrex(key, secret)
 
-threshold_activ = 0.4 # Number between -1 and 1
-threshold_off   = -0.3 # Number between -1 and 1
+threshold_activ = 0.1  # Number between -1 and 1
+threshold_off = -0.1  # Number between -1 and 1
 
-btc_vault = 0.0 # Btc vault
-usdt_vault = 10000 # Usd tether vault
+btc_vault = 0  # Btc vault
+usdt_vault = 10000  # Usd tether vault
+
+max_spread = 5
 
 sell_pos = 0
 buy_pos = 0
 
 while True:
 
-	results = bittrex.get_market_history('USDT-BTC',800)['result']
+    results = bittrex.get_market_history('USDT-BTC', 800)['result']
 
-	buys = 0
-	sells = 0
+    buys = 0
+    sells = 0
 
-	buys_size = 0
-	sells_size = 0
+    buys_size = 0
+    sells_size = 0
 
-	latest_price_bid = 0
-	latest_price_ask = 0
+    latest_price_bid = 0
+    latest_price_ask = 0
 
-	for i in results:
+    for i in results:
 
-		if i['OrderType'] == 'BUY':
-			buys += 1
-			buys_size += i['Quantity']
-			
-			if latest_price_ask == 0:
-				latest_price_ask = i['Price']
-		else:
-			sells += 1
-			sells_size += i['Quantity']
+        if i['OrderType'] == 'BUY':
+            buys += 1
+            buys_size += i['Quantity']
 
-			if latest_price_bid == 0:
-				latest_price_bid = i['Price']	
+            if latest_price_ask == 0:
+                latest_price_ask = i['Price']
+        else:
+            sells += 1
+            sells_size += i['Quantity']
 
-	# Strategy
-	
-	ratio = (buys_size - sells_size)/(buys_size+sells_size)
+            if latest_price_bid == 0:
+                latest_price_bid = i['Price']
 
-	# Check buy 
-	if ratio > threshold_activ:
-		btc_vault += usdt_vault/latest_price_ask
-		usdt_vault = 0
-		buy_pos = 1
+    # Strategy
 
-	# Check sell
-	if ratio < threshold_off:
-		usdt_vault += btc_vault*latest_price_bid
-		btc_vault = 0
-		sell_pos = 1
+    ratio = (buys_size - sells_size) / (buys_size + sells_size)
+    reasonable_spread = (latest_price_ask - latest_price_bid) < max_spread
 
+    # Check buy
+    if ratio > threshold_activ and reasonable_spread:
+        btc_vault += usdt_vault / latest_price_ask
+        usdt_vault = 0
+        buy_pos = 1
 
+    # Check sell
+    if ratio < threshold_off and reasonable_spread:
+        usdt_vault += btc_vault * latest_price_bid
+        btc_vault = 0
+        sell_pos = 1
 
-	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-	print(" Buys   : %10d %8f" % (buys,buys_size))
-	print(" Sells  : %10d %8f" % (sells,sells_size))
-	print(" Ratio  : %10f" % ratio)
-	print("Bitcoin price: BID:%10f ASK:%10f" % (latest_price_bid, latest_price_ask))
-	print("BTC vault: %8f USDT vault: %8f Total value: %8f" %(btc_vault,usdt_vault,usdt_vault + btc_vault * latest_price_bid))
-	print("")
-	time.sleep(60)
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print(" Buys   : %10d %8f" % (buys, buys_size))
+    print(" Sells  : %10d %8f" % (sells, sells_size))
+    print(" Ratio  : %10f" % ratio)
+    print("Bitcoin price: BID:%10f ASK:%10f" % (latest_price_bid, latest_price_ask))
+    print("BTC vault: %8f USDT vault: %8f Total value: %8f" % (
+        btc_vault, usdt_vault, usdt_vault + btc_vault * latest_price_bid))
+    print("")
+    time.sleep(30)
