f5365178392b63efc55a7ece7394009985be89c6
ipaolo <igor@igor-OptiPlex-7040>
2017-10-09 16:16:18 +0400

    Pushing history

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ requests.packages.urllib3.disable_warnings()
 import sqlite3
 
 conn = sqlite3.connect('bitcoin.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS history
+             (time INTEGER, bid REAL,ask REAL,bought REAL,sold REAL,buys INTEGER,sells INTEGER)''')
 
 key = "230cd2aaff354c43a3219a978ac2aebd"
 secret = "fe11c3a0a7d14020bb78554ceb67de76"
 
 bittrex = Bittrex(key, secret)
 
-threshold_activ = 0.1  # Number between -1 and 1
-threshold_off = -0.1  # Number between -1 and 1
+threshold_activ = 0.5  # Number between -1 and 1
+threshold_off = -0.5  # Number between -1 and 1
 
 btc_vault = 0  # Btc vault
 usdt_vault = 10000  # Usd tether vault
 
-max_spread = 5
+max_spread = 20
 
 sell_pos = 0
 buy_pos = 0
@@ -80,4 +84,7 @@ while True:
     print("BTC vault: %8f USDT vault: %8f Total value: %8f" % (
         btc_vault, usdt_vault, usdt_vault + btc_vault * latest_price_bid))
     print("")
+    
+    c.execute("INSERT INTO history (time,bid,ask,bought,sold,buys,sells) VALUES (%u, %f, %f, %f, %f, %d, %d)" % (time.time(),latest_price_bid,latest_price_ask,buys_size,sells_size,buys,sells ))
+
     time.sleep(30)
